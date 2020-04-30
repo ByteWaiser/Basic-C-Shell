43816c381515809242a8a9770ae66eddf234d0b8
ByteWaiser <bytewaiser@gmail.com>
2020-04-30 07:27:02 +0300

    Added builtin commands and cleared the file a bit

--- main.c
+++ main.c
@@ -9,36 +9,18 @@
 
 char error_message[30] = "An error has occurred\n";
 
-char *paths[] = {"/bin/"};
-
-int paths_size(){
-    return sizeof(paths) / sizeof(*paths);
+void builtin_error(){
+    write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-char *builtin_list[] = {"exit", "cd", "path"};
+char *paths[100] = {"/bin"};
 
-int exit_cmd(){
-    return 0;
-}
-
-int (*builtin_func[]) (char **) = {
-    &exit_cmd,
-};
-
-char *ltrim(char *s){
-    while(isspace(*s)) s++;
-    return s;
-}
-
-char *rtrim(char *s){
+char *trim(char *s){
     char* back = s + strlen(s);
+    while(isspace(*s)) s++;
     while(isspace(*--back));
     *(back+1) = '\0';
     return s;
-}
-
-char *trim(char *s){
-    return rtrim(ltrim(s));
     // last char will be strlen(s)-1
 }
 
@@ -52,54 +34,94 @@ char **sep(char *s){
     return temp;
 }
 
-int run(char **args){
+void run(char **args){
     pid_t pid, wpid;
     int status;
     char *full_path = malloc(sizeof(char)*100);
-
-    for(int i = 0; i < paths_size(); i++){
-        
-
-        strcat(strcpy(full_path, paths[i]), args[0]);
-        printf("%s\n", full_path);
-        if(access(full_path, X_OK) == 0) break;
+    char *final_path = malloc(sizeof(char)*100);
+    
+    int i = 0;
+    while(*(paths+i) != NULL){
+        full_path = strcat(strcat(strcpy(full_path, *(paths+i)),"/"), args[0]);
+        if(access(full_path, F_OK) == 0){
+            strcpy(final_path, full_path);
+        }
+        i++;
     }
-    
-    
-    
+
     pid = fork();
     if (pid == 0) {
         // Child process
         
-        if (execv(full_path, args) == -1) {
-            perror("cen354sh");
+        if (execv(final_path, args) == -1) {
+            builtin_error();
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
         // Error forking
-        printf("asdas\n");
+        builtin_error();
     } else {
         // Parent process
         do {
         wpid = waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
-    return 1;
+}
+
+void process(char **args){
+    if(args[0] == NULL){
+        return;
+    }
+
+    if(strcmp(args[0], "exit") == 0){
+        exit(0);
+    }
+    else if(strcmp(args[0], "cd") == 0){
+        if(args[1] == NULL || args[2] != NULL){
+            builtin_error();
+        }else{
+            if(chdir(args[1]) !=0) builtin_error();
+        }
+    }
+    else if(strcmp(args[0], "path") == 0){
+        if(args[1] == NULL){
+            paths[0] = NULL;
+        }
+        int i = 0;
+        while(*(args+i+1) != NULL){
+            *(paths+i) = *(args + i + 1);
+            printf("%s\n", paths[i]);
+            i++;
+        }
+    }
+    else if(paths[0] == NULL){
+        builtin_error();
+    }
+    else{
+        run(args);
+    }
+}
+
+void shell_loop(){
+
+    char *input;
+    char **tokens;
+    size_t size = 0;
+    while(1){
+        printf(">>> ");
+        getline(&input, &size, stdin);
+        input = trim(input);
+        tokens = sep(input);
+        process(tokens);
+        free(input);
+        free(tokens);
+    }
+
 }
 
 int main(){
 
-    while(1){
-        char *s;
-        size_t a = 0;
-        s = malloc(sizeof(char) * a);
-        printf(">>> ");
-        int x = getline(&s, &a, stdin);
-        char** k;
-        s = trim(s);
-        k = sep(s);
-        run(k);
-    }
+    shell_loop();
 
     return 0;
 }
